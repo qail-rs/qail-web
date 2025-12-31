de11a5442f5124e2cff30535f8589f99d3c00516
verysignvl <qvilinx@gmail.com>
2026-01-01 00:59:12 +0800

    docs: update pg-driver docs with LRU cache limit 100

--- public/docs/searcher-c2a407aa.js
+++ public/docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-10c225ba.js',
+                path_to_root + 'searchindex-e82c8622.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
