32d620475876449eea86cb821b19f4b1825f37a8
verysignvl <qvilinx@gmail.com>
2026-01-01 11:14:53 +0800

    docs: Update documentation content and regenerate search index.

--- public/docs/searcher-c2a407aa.js
+++ public/docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e82c8622.js',
+                path_to_root + 'searchindex-8b4a691a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
